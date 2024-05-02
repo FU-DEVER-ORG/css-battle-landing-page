0a7af691c241698519883dfcba0d21079733a27d
Garchomppy <chontem123@gmail.com>
2024-05-02 23:11:22 +0700

    ContactSection

--- src/components/sections/ContactSection/index.jsx
+++ src/components/sections/ContactSection/index.jsx
@@ -4,7 +4,11 @@ function ContactSection() {
   return (
     <section className={styles['contact']}>
       <div className="container">
-        <h3 className={styles['contact__title']}>contact</h3>
+        <h3 className={styles['contact__title']}>Contact</h3>
+        <h3 className={styles['contact__title']}>Contact</h3>
+        <h3 className={styles['contact__title']}>Contact</h3>
+        <h3 className={styles['contact__title']}>Contact</h3>
+
         <div className={styles['contact__image']}>Image</div>
       </div>
     </section>
