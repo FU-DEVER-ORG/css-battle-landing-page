4c557bcdb7a76a99630f1007c104e2dd287de9a2
Garchomppy <chontem123@gmail.com>
2024-05-06 09:27:09 +0700

    thay doi retro va style

--- src/components/sections/CompetitionStruct/index.jsx
+++ src/components/sections/CompetitionStruct/index.jsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import styles from "./styles.module.scss";
+import Image from 'next/image';
 import styles from './styles.module.scss';
 
 function CompetitionStruct() {
@@ -12,3 +15,28 @@ function CompetitionStruct() {
 }
 
 export default CompetitionStruct;
+export function Retro() {
+  return (
+    <section className={styles['contact']}>
+      <div className="container">
+        <div className={styles['contact__intro']}>
+
+          <h3 className={styles['contact__intro1']}>RETRO</h3>
+          <h4 className={styles['contact__intro2']}>Cảm nghĩ</h4>
+          {/* <div className={styles['contact__line']}></div>           */}
+        </div>
+
+        <div className={styles['contact__outimage']}>
+
+          <Image src="/imga/image5.jpg" className={styles['contact__image']} width={400} height={400} alt="Image5" />
+          <Image src="/imga/image7.jpg" className={styles['contact__image']} width={400} height={400} alt="Image7" />
+          <Image src="/imga/image6.jpg" className={styles['contact__image']} width={400} height={400} alt="Image6" />
+
+        </div>
+
+        <h3 className={styles['contact__title']}>Cảm nghĩ của những chiến binh CSS Battle 2023</h3>
+
+      </div>
+    </section>
+  );
+}
