636ddbd4947b5f92e3f1963cb4dbbc128d6bc0f4
ldanh270 <ducanhle.dn@gmail.com>
2024-05-06 23:01:33 +0700

    them logo menu bar

--- src/components/core/layouts/LandingLayout/Header/index.jsx
+++ src/components/core/layouts/LandingLayout/Header/index.jsx
@@ -1,10 +1,22 @@
+/* eslint-disable @next/next/no-img-element */
 import styles from './styles.module.scss';
+import Image from 'next/image';
 
 function Header() {
   return (
     <header className={styles['header']}>
       <div className="container">
-        <div>Hero Section</div>
+        <div id="logo">
+          <img
+            id="logo-logo"
+            src="logo-clb.jpg"
+            width={500}
+            height={500}
+            alt="logoclb"
+          />
+          <span id="text-logo">CSS Battle</span>
+          <span id="year-logo">--2024--</span>
+        </div>
       </div>
     </header>
   );
