829eebf8a6b28f1c1f874977ec69b708b0ac9ccc
Garchomppy <chontem123@gmail.com>
2024-05-07 21:32:45 +0700

    fix anh va them gui mobile

--- src/components/sections/Retro/index.jsx
+++ src/components/sections/Retro/index.jsx
@@ -1,8 +1,8 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
-import Top2 from "@public/Retro/player2.png";
-import Top1 from '@public/Retro/player1.png';
-import Top3 from '@public/Retro/player3.png';
+import Player1 from '@/public/cam_nghi/player1.png';
+import Player2 from '@/public/cam_nghi/player2.png';
+import Player3 from '@/public/cam_nghi/player3.png';
 
 import * as  styles from './styles.module.scss';
 
@@ -14,23 +14,23 @@ function Retro() {
 
           <h3 className={styles['contact__intro1']}>RETRO</h3>
           <h3 className={styles['contact__intro2']}>Cảm nghĩ</h3>
-          
+            
        </div>
 
         <div className={styles['contact__outimage']}>
 
           <Image
           className={styles['contact__image']} width={400} height={400} 
-          src="Top2"
+          src={Player2}
           alt="Top2" />
 
           <Image 
-          src="Top1" 
+          src={Player1}
           className={styles['contact__image']} width={400} height={400} 
           alt="Top1" />
 
           <Image 
-          src="Top3" 
+          src={Player3}
           className={styles['contact__image']} width={400} height={400} 
           alt="Top3" />
         
