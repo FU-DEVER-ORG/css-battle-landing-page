196cdd50250d9385b146581f118ec848064d432e
Hoangphuong2k5 <phuongnguyen200504@gmail.com>
2024-05-09 22:52:55 +0700

    introduction thieu kieu chu

--- src/components/sections/Introduction/index.jsx
+++ src/components/sections/Introduction/index.jsx
@@ -8,15 +8,14 @@ function Introduction() {
          <h3 className={styles['contact__title1']}>CSS BATTLE 2024 LÀ GÌ?</h3>
          <h3 className={styles['contact__title2']}>Thông tin về cuộc thi</h3>
          </div>
-         <div className={styles['contact__image']}>
-           <div>
-            <img src="/photo/introduction1.png" alt="CSS battle" />
-           <div> <p className={styles['contact__content1']}>CSS Battle 2024 là cuộc thi dành cho sinh viên của Đại học FPT Đà Nẵng.Sau sự thành công của mùa 1 tổ chức vào năm 2023, 
-CSS Battle mùa 2 hứa hẹn sẽ là sân chơi rực  dành cho những ai có đam mê trong lĩnh vực HTML/CSS</p> </div>
+          <div>
+            <img  src="/photo/introduction1.png" alt="CSS battle" />
+            <p className={styles['contact__content1']}>CSS Battle 2024 là cuộc thi dành cho sinh viên của Đại học FPT Đà Nẵng.Sau sự thành công của mùa 1 tổ chức vào năm 2023, 
+CSS Battle mùa 2 hứa hẹn sẽ là sân chơi rực  dành cho những ai có đam mê trong lĩnh vực HTML/CSS</p> 
             <img src="/photo/introduction2.png" alt="CSS battle event" />
             <p className={styles['contact__content2']}>Cuộc thi sẽ yêu cầu bạn sử dụng HTML/CSS để tạo ra các kết quả giống đề bài nhất có thể trong thời gian ngắn nhất</p>
           </div>
-         </div>
+         
          
       </div>
     </section>
