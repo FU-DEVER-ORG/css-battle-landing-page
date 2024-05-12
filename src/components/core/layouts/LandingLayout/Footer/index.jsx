6443b84c04ae4f51c35bb0eda9b6f717825268fe
ldanh270 <ducanhle.dn@gmail.com>
2024-05-13 00:58:05 +0700

    done - con thieu link form dien phan dang ky ngay

--- src/components/core/layouts/LandingLayout/Footer/index.jsx
+++ src/components/core/layouts/LandingLayout/Footer/index.jsx
@@ -1,9 +1,19 @@
 import styles from './styles.module.scss';
+import Image from 'next/image';
 
 function Footer() {
   return (
     <footer className={styles['footer']}>
-      <div className="container">Đây là footer</div>
+      <div className={styles['tittle']}>
+        Đăng ký tham gia CSS Battle 2024 để chinh phục ngôi vương “The king of
+        CSS 2024”!
+      </div>
+      <a href="" className={styles['register']}>
+        Đăng ký ngay
+      </a>
+      <div className={styles['footer_tittle']}></div>
+      <div className={styles['footer_tittle']}></div>
+      <div className={styles['footer_tittle']}></div>
     </footer>
   );
 }
--- src/components/core/layouts/LandingLayout/Header/index.jsx
+++ src/components/core/layouts/LandingLayout/Header/index.jsx
@@ -3,25 +3,27 @@ import styles from './styles.module.scss';
 function Header() {
   return (
     <header className={styles['header']}>
-      <div className={styles['logo']}>
+      <a href="#HeroSection" className={styles['logo']}>
         <div className={styles['logo_element_1']}>CSS BATTLE</div>
         <div className={styles['logo_element_2']}>--2024--</div>
-      </div>
+      </a>
       <nav className={styles['menubar']}>
-        <a href="" className={styles['menubar_element']}>
+        <a href="#Introduction" className={styles['menubar_element']}>
           Giới thiệu
         </a>
-        <a href="" className={styles['menubar_element']}>
+        <a href="#Prizes" className={styles['menubar_element']}>
           Giải thưởng
         </a>
-        <a href="" className={styles['menubar_element']}>
+        <a href="#TimelineSection" className={styles['menubar_element']}>
           Timeline cuộc thi
         </a>
-        <a href="" className={styles['menubar_element']}>
+        <a href="#CompetitionForm" className={styles['menubar_element']}>
           Thể lệ thi
         </a>
       </nav>
-      <button className={styles['register']}>Đăng ký ngay</button>
+      <a href="" className={styles['register']}>
+        Đăng ký ngay
+      </a>
     </header>
   );
 }
