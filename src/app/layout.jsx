0e2a56d578cb45873d38feb19c6c9c8983a43496
Hoangphuong2k5 <phuongnguyen200504@gmail.com>
2024-05-13 11:56:55 +0700

    prizes( thiếu giải thưởng)

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,10 @@
-import { Inter } from 'next/font/google';
+import { Oswald } from 'next/font/google';
 
 import LandingLayout from '@/components/core/layouts/LandingLayout';
 
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const oswald = Oswald ({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'FU-DEVER | CSS Battle 2024',
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={oswald.className}>
         <LandingLayout>{children}</LandingLayout>
       </body>
     </html>
--- src/components/sections/Prizes/index.jsx
+++ src/components/sections/Prizes/index.jsx
@@ -4,8 +4,15 @@ function Prizes() {
   return (
     <section className={styles['contact']}>
       <div className="container">
-        <h3 className={styles['contact__title']}>Giải thưởng</h3>
-        <div className={styles['contact__image']}>Image</div>
+        <div className={styles['contact__title']}>
+        <h3 className={styles['contact__title1']}>CƠ CẤU GIẢI THƯỞNG</h3>
+        <h3 className={styles['contact__title2']}>Phần thưởng đầy hấp dẫn cho các chiến binh</h3>
+        </div>
+       <div > 
+        <img src="/photo prizes/Rectangle 143.png" alt="prizes" className={styles['image']} />
+        <img src="/photo prizes/Rectangle 142.png" alt="prizes" className={styles['image1']} />
+        <img src="/photo prizes/Rectangle 141.png" alt="prizes" className={styles['image2']} />
+       </div>
       </div>
     </section>
   );
