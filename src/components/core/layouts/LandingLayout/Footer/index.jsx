aff05a4c4599687813f2cf603ab68e86310d8dd2
ldanh270 <ducanhle.dn@gmail.com>
2024-05-14 12:15:09 +0700

    clean

--- src/components/core/layouts/LandingLayout/Footer/index.jsx
+++ src/components/core/layouts/LandingLayout/Footer/index.jsx
@@ -4,16 +4,7 @@ import Image from 'next/image';
 function Footer() {
   return (
     <footer className={styles['footer']}>
-      <div className={styles['tittle']}>
-        Đăng ký tham gia CSS Battle 2024 để chinh phục ngôi vương “The king of
-        CSS 2024”!
-      </div>
-      <a href="" className={styles['register']}>
-        Đăng ký ngay
-      </a>
-      <div className={styles['footer_tittle']}></div>
-      <div className={styles['footer_tittle']}></div>
-      <div className={styles['footer_tittle']}></div>
+      <div className="container">Đây là footer</div>
     </footer>
   );
 }
