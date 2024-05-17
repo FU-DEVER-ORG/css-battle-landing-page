1269243495f26b73eb128e6df7af8c70024e777b
Hoangphuong2k5 <phuongnguyen200504@gmail.com>
2024-05-17 11:55:08 +0700

    chinh sua

--- src/components/sections/Prizes/index.jsx
+++ src/components/sections/Prizes/index.jsx
@@ -2,10 +2,33 @@ import styles from './styles.module.scss';
 
 function Prizes() {
   return (
-    <section className={styles['contact']}>
+    <section className={styles.contact}>
       <div className="container">
-        <h3 className={styles['contact__title']}>Giải thưởng</h3>
-        <div className={styles['contact__image']}>Image</div>
+        <div className={styles.contact__title}>
+          <h3 className={styles.contact__title1}>CƠ CẤU GIẢI THƯỞNG</h3>
+          <h3 className={styles.contact__title2}>Phần thưởng đầy hấp dẫn cho các chiến binh</h3>
+        </div>
+        <div>
+          <img src="/photo prizes/Rectangle 143.png" alt="prizes" className={styles.image} />
+          <img src="/photo prizes/Rectangle 142.png" alt="prizes" className={styles.image1} />
+          <img src="/photo prizes/Rectangle 141.png" alt="prizes" className={styles.image2} />
+        </div>
+        <div className={styles.boxContainer}>
+          <div className={styles.box}>
+            <div className={styles.box__box1}>
+              <h4 className={styles.top}>Giải nhì</h4>
+              <h4 className={styles.award}>1.500.000 VNĐ</h4>
+            </div>
+            <div className={styles.box__box2}>
+              <h4 className={styles.top}>Giải nhất</h4>
+              <h4 className={styles.award}>2.000.000 VNĐ</h4>
+            </div>
+            <div className={styles.box__box3}>
+              <h4 className={styles.top}>Giải ba</h4>
+              <h4 className={styles.award}>1.000.000 VNĐ</h4>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
