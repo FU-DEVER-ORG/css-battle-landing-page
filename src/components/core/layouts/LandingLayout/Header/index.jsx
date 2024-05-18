5477d612a090684d09d0ebf23509d6596a58cd82
ldanh270 <ducanhle.dn@gmail.com>
2024-05-18 21:49:46 +0700

    done desktop, dang code mobile

--- src/components/core/layouts/LandingLayout/Header/index.jsx
+++ src/components/core/layouts/LandingLayout/Header/index.jsx
@@ -4,49 +4,37 @@ import { FaTimes } from 'react-icons/fa';
 
 function Header() {
   return (
-    <header className={styles['header']}>
-      <div className={styles['over_play']}></div>
-      <a href="#HeroSection" className={styles['logo']}>
-        <div className={styles['logo_element_1']}>CSS BATTLE</div>
-        <div className={styles['logo_element_2']}>--2024--</div>
-      </a>
-      <nav className={styles['menubar_pc']}>
-        <a href="#Introduction" className={styles['menubar_pc_element']}>
-          Giới thiệu
+    <header>
+      <div className={styles['header']}>
+        <a href="#HeroSection" className={styles['logo']}>
+          <div className={styles['logo_element_1']}>CSS BATTLE</div>
+          <div className={styles['logo_element_2']}>--2024--</div>
         </a>
-        <a href="#Prizes" className={styles['menubar_pc_element']}>
-          Giải thưởng
+        <nav className={styles['menubar']}>
+          <a href="#Introduction" className={styles['menubar_element']}>
+            Giới thiệu
+          </a>
+          <a href="#Prizes" className={styles['menubar_element']}>
+            Giải thưởng
+          </a>
+          <a href="#TimelineSection" className={styles['menubar_element']}>
+            Timeline cuộc thi
+          </a>
+          <a href="#CompetitionForm" className={styles['menubar_element']}>
+            Thể lệ thi
+          </a>
+        </nav>
+        <a
+          href="https://docs.google.com/forms/d/e/1FAIpQLSc1KNczkcH68Uxgk09WR6eEpbqUyk5ASEVzLDFCZGKNA3pQ7A/viewform?usp=sf_link"
+          className={styles['register']}
+        >
+          Đăng ký ngay
         </a>
-        <a href="#TimelineSection" className={styles['menubar_pc_element']}>
-          Timeline cuộc thi
-        </a>
-        <a href="#CompetitionForm" className={styles['menubar_pc_element']}>
-          Thể lệ thi
-        </a>
-      </nav>
-      <FaBars className={styles['nav_show']} />
-      <nav className={styles['menubar_mobile']}>
-        <FaTimes className={styles['nav_close']} />
-
-        <a href="#Introduction" className={styles['menubar_mobile_element']}>
-          Giới thiệu
-        </a>
-        <a href="#Prizes" className={styles['menubar_mobile_element']}>
-          Giải thưởng
-        </a>
-        <a href="#TimelineSection" className={styles['menubar_mobile_element']}>
-          Timeline cuộc thi
-        </a>
-        <a href="#CompetitionForm" className={styles['menubar_mobile_element']}>
-          Thể lệ thi
-        </a>
-      </nav>
-      <a
-        href="https://docs.google.com/forms/d/e/1FAIpQLSc1KNczkcH68Uxgk09WR6eEpbqUyk5ASEVzLDFCZGKNA3pQ7A/viewform?usp=sf_link"
-        className={styles['register']}
-      >
-        Đăng ký ngay
-      </a>
+      </div>
+      <div className={styles['icon']}>
+        <FaBars className={styles['icon_open']} />
+        <FaTimes className={styles['icon_close ']} />
+      </div>
     </header>
   );
 }
--- src/components/sections/HeroSection/index.jsx
+++ src/components/sections/HeroSection/index.jsx
@@ -3,6 +3,7 @@ import styles from './styles.module.scss';
 function HeroSection() {
   return (
     <section className={styles['hero']}>
+      <div className={styles['hero_gap_box']}></div>
       <div className="container">
         <h3 className={styles['hero__title']}>Hero</h3>
         <div className={styles['hero__image']}>Image</div>
