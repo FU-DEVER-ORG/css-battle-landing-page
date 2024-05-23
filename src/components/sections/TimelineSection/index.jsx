fe47dc280b58b35f9a949e258e2476a491a49247
Garchomppy <chontem123@gmail.com>
2024-05-23 10:36:09 +0700

    soonar cloud

--- src/components/sections/TimelineSection/index.jsx
+++ src/components/sections/TimelineSection/index.jsx
@@ -1,30 +1,24 @@
-
 import * as styles from './styles.module.scss';
 
 function TimelineSection() {
   return (
     <section className={styles['timeline']}>
       <div className="container">
-
         <div className={styles['timeline__frame']}>
           <div className={styles['timeline__hinh']}></div>
-
           <div className={styles['timeline__title']}>
             <div className={styles['timeline__title1']}>TIMELINE CUỘC THI</div>
             <span className={styles['timeline__title2']}>Các giai đoạn cuộc thi</span>
           </div>
         </div>
-
         <div className={styles['timeline__group']}>
           <div className={styles['timeline__container-1']}>
-
             <div className={styles['timeline__group-up1']}>
               <div className={styles['timeline__circle-1']}>
                 <span className={styles['timeline__number_1']}>1</span>
               </div>
               <div className={styles['timeline__line1']}></div>
             </div>
-
             <div className={styles['timeline__group-up2']}>
               <div className={styles['timeline__date1']}>09/06</div>
               <span className={styles['timeline__content_1']}>
@@ -33,14 +27,12 @@ function TimelineSection() {
                 </p>
               </span>
             </div>
-
             <div className={styles['timeline__group-up3']}>
               <div className={styles['timeline__circle-2']}>
                 <span className={styles['timeline__number_2']}>3</span>
               </div>
               <div className={styles['timeline__line2']}></div>
             </div>
-
             <div className={styles['timeline__group-up4']}>
               <div className={styles['timeline__date2']}>23/06</div>
               <span className={styles['timeline__content_1']}>
@@ -49,38 +41,28 @@ function TimelineSection() {
                 </p>
               </span>
             </div>
-
           </div>
-
           <div className={styles['timeline__container-2']}>
-
             <div className={styles['timeline__bar1']}>
-
               <div className={styles['timeline__bar3']}>
                 <div className={styles['timeline__rectangle1']}></div>
                 <div className={styles['timeline__triangle1']}></div>
               </div>
-
               <div className={styles['timeline__bar2']}>
                 <div className={styles['timeline__triangle2']}></div>
                 <div className={styles['timeline__rectangle2']}></div>
               </div>
-
               <div className={styles['timeline__bar3']}>
                 <div className={styles['timeline__rectangle3']}></div>
                 <div className={styles['timeline__triangle3']}></div>
               </div>
-
               <div className={styles['timeline__bar2']}>
                 <div className={styles['timeline__triangle4']}></div>
                 <div className={styles['timeline__rectangle4']}></div>
               </div>
-
             </div>
           </div>
-
           <div className={styles['timeline__container-3']}>
-
             <div className={styles['timeline__group-up6']}>
               <div className={styles['timeline__date3']}>25/05 - 05/06</div>
               <span className={styles['timeline__content_1']}>
@@ -89,14 +71,12 @@ function TimelineSection() {
                 </p>
               </span>
             </div>
-
             <div className={styles['timeline__group-up5']}>
               <div className={styles['timeline__line3']}></div>
               <div className={styles['timeline__circle-3']}>
                 <span className={styles['timeline__number_3']}>2</span>
               </div>
             </div>
-
             <div className={styles['timeline__group-up7']}>
               <div className={styles['timeline__date4']}>16/06</div>
               <span className={styles['timeline__content_1']}>
@@ -105,21 +85,16 @@ function TimelineSection() {
                 </p>
               </span>
             </div>
-
             <div className={styles['timeline__group-up8']}>
               <div className={styles['timeline__line4']}></div>
               <div className={styles['timeline__circle-4']}>
                 <span className={styles['timeline__number_4']}>4</span>
               </div>
             </div>
-
           </div>
-
         </div>
-    
       </div>
     </section>
-
   );
 }
 
