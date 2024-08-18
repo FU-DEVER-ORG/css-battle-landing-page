8c67c4f27b4ba9a86d8180823fb262697e1af23f
ldanh270 <ducanhle.dn@gmail.com>
2024-08-18 19:44:40 +0700

    commit

--- src/components/sections/HeroSection/index.jsx
+++ src/components/sections/HeroSection/index.jsx
@@ -1,13 +1,17 @@
 import styles from './styles.module.scss';
+import Image from 'next/image';
 
 function HeroSection() {
   return (
     <section className={styles['hero']}>
-      <div className={styles['hero_gap_box']}></div>
-      <div className="container">
-        <h3 className={styles['hero__title']}>Hero</h3>
-        <div className={styles['hero__image']}>Image</div>
+      <div className={styles['cup']}>
+        {/* <img src="/hero_section/cup.png" alt="Cup" /> */}
       </div>
+      <div className={styles['title']}>
+        <div className={styles['title_1']}>CSS BATTLE 2024</div>
+        <div className={styles['title_2']}>Finding the king of CSS</div>
+      </div>
+      <div className="founder">Tổ chức bởi FU-DEVER</div>
     </section>
   );
 }
